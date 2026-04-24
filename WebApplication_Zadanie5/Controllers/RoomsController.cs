1674cd8d88fbab2254f170742016c9cb7ca52d7e
Volodymyr Hodovanets <s29985@pjwstk.edu.pl>
2026-04-24 11:15:51 +0200

    first commit

---
+++ WebApplication_Zadanie5/Controllers/RoomsController.cs
@@ -0,0 +1,6 @@
+namespace WebApplication_Zadanie5.Controllers;
+
+public class RoomsController
+{
+    
+}
---
+++ WebApplication_Zadanie5/Models/Reservation.cs
@@ -0,0 +1,6 @@
+namespace WebApplication_Zadanie5.Models;
+
+public class Reservation
+{
+    
+}
